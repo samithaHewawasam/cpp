4ffe24610504b0b94855ea61107ebb09aa7e4194
Samitha Hewawasam <eupdate@users.noreply.github.com>
2015-06-05 19:36:44 +0530

    assending file formatted

--- ascending.cpp
+++ ascending.cpp
@@ -11,43 +11,43 @@
 #include <iostream>
 
 
-void main(){
+void main() {
 
 //let's get 3 integer varibles to hold 3 numbers
-int a,b,c;
+    int a,b,c;
 
 //ask from user to enter those
-cout<<"Enter your 1st number?\n";
-cin>>a;
+    cout<<"Enter your 1st number?\n";
+    cin>>a;
 
-cout<<"Enter your 2nd number?\n";
-cin>>b;
+    cout<<"Enter your 2nd number?\n";
+    cin>>b;
 
-cout<<"Enter your 3rd number?\n";
-cin>>c;
+    cout<<"Enter your 3rd number?\n";
+    cin>>c;
 
-cout<<"\nAscending Order is bellow\n";
+    cout<<"\nAscending Order is bellow\n";
 
-    if(a < b && a < c){
-      cout<<a<<endl;
-      if(b < c){
-        cout<<b<<endl;
-        cout<<c<<endl;
-      }
-    }
-    else if(b < a && b < c){
-      cout<<b<<endl;
-      if(a < c){
+    if(a < b && a < c) {
         cout<<a<<endl;
-        cout<<c<<endl;
-      }
+        if(b < c) {
+            cout<<b<<endl;
+            cout<<c<<endl;
+        }
     }
-    else if(c < a && c < b){
-      cout<<c<<endl;
-      if(a < b){
-        cout<<a<<endl;
+    else if(b < a && b < c) {
         cout<<b<<endl;
-      }
+        if(a < c) {
+            cout<<a<<endl;
+            cout<<c<<endl;
+        }
+    }
+    else if(c < a && c < b) {
+        cout<<c<<endl;
+        if(a < b) {
+            cout<<a<<endl;
+            cout<<b<<endl;
+        }
     }
 
 }
